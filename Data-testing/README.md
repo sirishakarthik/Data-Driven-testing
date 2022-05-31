31eee2ed71e65281ecb04fba7d88a1dcd9bca31f
saisirishasmail@gmail.com <saisirishasmail@gmail.com>
2022-05-31 15:42:07 -0400

    excel data

---
+++ Data-testing/README.md
@@ -0,0 +1,1 @@
+## Data-testing Java Project
---
+++ Data-testing/src/main/java/DataDriven/Data_testing/Data-testing/Application.java
@@ -0,0 +1,9 @@
+package DataDriven.Data_testing.Data-testing;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Data-testing");
+    }
+
+}
