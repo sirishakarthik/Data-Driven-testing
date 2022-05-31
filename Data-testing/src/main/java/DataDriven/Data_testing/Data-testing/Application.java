213626b3e7248094e6c791d41928f3987dddbf9a
sirishakarthik <saisirishasmail@gmail.com>
2022-05-31 15:45:53 -0400

    Update Application.java

--- Data-testing/src/main/java/DataDriven/Data_testing/Data-testing/Application.java
+++ Data-testing/src/main/java/DataDriven/Data_testing/Data-testing/Application.java
@@ -3,7 +3,7 @@ package DataDriven.Data_testing.Data-testing;
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello Data-testing");
+        System.out.println("Hello Data-testing....");
     }
 
 }
